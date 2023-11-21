0f7048c468b32189689a5042e2af97970a8ea228
vyPal <66716025+vyPal@users.noreply.github.com>
2023-11-21 10:42:39 +0100

    Remoe default newline at print

--- src/compiler/functions.go
+++ src/compiler/functions.go
@@ -121,16 +121,16 @@ func (c *Context) compilePrintCall(s SPrint) {
 	var formatString string
 	switch t := value.Type().(type) {
 	case *types.IntType:
-		formatString = "%d\n"
+		formatString = "%d"
 	case *types.FloatType:
-		formatString = "%f\n"
+		formatString = "%f"
 	case *types.ArrayType: // Assuming strings are represented as an array of characters
 		if t.ElemType.Equal(types.I8) {
 			// Create a global constant for the string
 			str := c.Module.NewGlobalDef(".str", value.(constant.Constant))
 			// Get a pointer to the first element of the string
 			value = c.Block.NewGetElementPtr(str.Type(), str, constant.NewInt(types.I32, 0))
-			formatString = "%s\n"
+			formatString = "%s"
 		} else {
 			panic(fmt.Errorf("cannot print value of type `%s`", value.Type()))
 		}
@@ -142,11 +142,11 @@ func (c *Context) compilePrintCall(s SPrint) {
 			c.Block.NewStore(value, str)
 			// Get a pointer to the first element of the string
 			value = c.Block.NewLoad(value.Type(), str)
-			formatString = "%s\n"
+			formatString = "%s"
 		} else if t.ElemType.Equal(types.I64) {
 			// Get a pointer to the first element of the string
 			value = c.Block.NewLoad(value.Type(), value)
-			formatString = "%d\n"
+			formatString = "%d"
 		} else {
 			panic(fmt.Errorf("cannot print value of type `%s`", value.Type()))
 		}
