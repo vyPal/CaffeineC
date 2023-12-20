05561d6548315ee6ac0ad3951d7b3d30e286037d
GitHub Action <action@github.com>
2023-12-20 06:18:42 +0000

    Update version to 2.0.8

--- src/main.go
+++ src/main.go
@@ -28,7 +28,7 @@ func main() {
 		EnableBashCompletion:   true,
 		Suggest:                true,
 		UseShortOptionHandling: true,
-		Version:                "2.0.7",
+		Version: "2.0.8",
 		Commands: []*cli.Command{
 			{
 				Name:  "build",
