4f401ed5d90739d6afc3978dbc38502407095eac
GitHub Action <action@github.com>
2024-04-02 07:30:02 +0000

    Update version to 2.2.29

--- src/main.go
+++ src/main.go
@@ -15,7 +15,7 @@ func main() {
 		EnableBashCompletion:   true,
 		Suggest:                true,
 		UseShortOptionHandling: true,
-		Version:                "2.2.28",
+		Version:                "2.2.29",
 		Commands:               commands,
 	}
 
