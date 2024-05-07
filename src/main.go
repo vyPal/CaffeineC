abf64b7a1981b25804fa7ee8bc12064b2480d070
GitHub Action <action@github.com>
2024-05-07 18:39:00 +0000

    Update version to 3.0.1

--- src/main.go
+++ src/main.go
@@ -15,7 +15,7 @@ func main() {
 		EnableBashCompletion:   true,
 		Suggest:                true,
 		UseShortOptionHandling: true,
-		Version:                "3.0.0",
+		Version:                "3.0.1",
 		Commands:               commands,
 	}
 
